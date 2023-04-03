057050491a6ccae0e79f254e460c3913d407648a
KyrylOTheRylo <kiril.mosiychuk@gmail.com>
2023-04-03 19:16:27 +0300

    Working ver 1.0

--- hw4.4.23/main.py
+++ hw4.4.23/main.py
@@ -1,10 +1,19 @@
 import numpy as np
 
-per = np.linspace(0, 5, 6)
-A = 0.5
+per = np.linspace(0, 8, 9)
+A = 0.228
 expon1 = lambda x, a: a ** x
 tmp = np.zeros((len(per), 2))
-for n, x in enumerate(per):
-    tmp[n][0] = -expon1(x, A)
-    tmp[n][1] = expon1(x, A)
-print(tmp)
+tmp1 = np.zeros((len(per), 2))
+sum1 = np.zeros((1, 2))
+for n, t in enumerate(per):
+    tmp[n][0] = -expon1(t, A)
+    tmp[n][1] = expon1(t, A)
+
+    s = 0
+    while s <= t - 1:
+        tmp1[n][0] += -expon1(t - s, A)
+        tmp1[n][1] += expon1(t - s, A)
+        s += 1
+for x in range(tmp.shape[0]):
+    print(f"{tmp[x]}\t{tmp1[x]}\t{tmp[x]+tmp1[x]}")
